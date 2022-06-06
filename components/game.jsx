435dded47096d1a9c66f01c44995ce28d6b49b3d
Kevin <NateraKevin1@gmail.com>
2022-06-06 19:53:37 -0400

    completed phase 1

--- components/game.jsx
+++ components/game.jsx
@@ -7,7 +7,23 @@ export default class Game extends React.Component {
       this.state = {
         board: new Minesweeper.Board(9, 10)
       };
+      
+      this.updateGame = this.updateGame.bind(this);
+    }
+    
+    updateGame() {
 
     }
 
+
+    render() {
+        return (
+            <div>
+                <h1>Minesweeper</h1>
+                <h2>Click to explore a tile.</h2>
+                <h2>Alt + click to flag a tile.</h2>
+                <Board board={this.state.board} updateGame={this.updateGame} />
+            </div>)
+        
+    }
 }
